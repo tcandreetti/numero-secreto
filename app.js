e505b9d465bd7ada51915f380fc499dfc036bed6
Thais Andreetti <thaiscandreetti@gmail.com>
2025-01-17 12:16:47 +0100

    alteração da mensagem

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ exibirMensagemInicial()
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('h1', 'Parabéns!!');
         let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`; 
         exibirTextoNaTela('p', mensagemTentativas);
